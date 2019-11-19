d1e88be219be8c6b6efa8f4c3a949ebd1b1ab773
Stephen Dade <stephen_dade@hotmail.com>
2019-11-19 16:18:42 +1100

    EEPROM: Updates

--- EEPROM/writeEEPROM.sh
+++ EEPROM/writeEEPROM.sh
@@ -8,5 +8,7 @@
 cd ./hats/eepromutils
 make
 cd ../../
-./hats/eepromutils/eepmake Pi-Connect-v1.txt Pi-Connect-v1.bin
-sudo ./hats/eepromutils/eepflash.sh -w -f=Pi-Connect-v1.bin -t=24c32 -d=0 -a=50
+./hats/eepromutils/eepmake Pi-Connect-Lite-v1.txt Pi-Connect-Lite-v1.bin
+sudo ./hats/eepromutils/eepflash.sh -w -f=Pi-Connect-Lite-v1.bin -t=24c32 -d=0 -a=50
+
+#Verify via: ls /proc/device-tree/hat/
