a8eaa2aa4390c8684c85761ce4043aa66a5ab887
Stephen Dade <stephen_dade@hotmail.com>
2023-08-18 16:06:45 +1000

    EEPROM: Add V2 board

--- EEPROM/writeEEPROM.sh
+++ EEPROM/writeEEPROM.sh
@@ -8,7 +8,7 @@
 cd ./hats/eepromutils
 make
 cd ../../
-./hats/eepromutils/eepmake Pi-Connect-Lite-v1.txt Pi-Connect-Lite-v1.bin
-sudo ./hats/eepromutils/eepflash.sh -w -f=Pi-Connect-Lite-v1.bin -t=24c32 -d=0 -a=50
+./hats/eepromutils/eepmake Pi-Connect-Lite-v2.txt Pi-Connect-Lite-v2.bin
+sudo ./hats/eepromutils/eepflash.sh -w -f=Pi-Connect-Lite-v2.bin -t=24c32 -d=0 -a=50
 
 #Verify via: ls /proc/device-tree/hat/, then "cat" the folders
